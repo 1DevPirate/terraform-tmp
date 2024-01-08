4dab4acadcaaef9778dd3eb0b0c97cb8bedd98d9
Admin <admin@Admins-MacBook-Air.local>
2024-01-08 11:27:58 -0800

    setting up deployment

--- main.tf
+++ main.tf
@@ -3,15 +3,6 @@ resource "aws_key_pair" "autodeploy" {
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
 
-resource "aws_security_group_rule" "restrict_ssh" {
-  security_group_id = "${aws_security_group.this.id}"
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["207.81.214.43/32"]
-}
-
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
