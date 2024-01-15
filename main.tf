0d451f738aa9f3498843f6f71bdc81f81b2ae65e
alfonsoh <alfonso@tekperfect.com>
2024-01-15 10:54:22 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_key_pair" "autodeploy" {
   key_name   = "autodeploy"  # Set a unique name for your key pair
 # Uncomment if you are running terraform with Jenkins
-  public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
+@  public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 # Uncomment if you are running terraform stand alone
-#  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_security_group" "ssh_access" {
